a934b2a1c8c357c071806a4a01a553b73f0785af
flaborde <fabrice.laborde@gmail.com>
2018-02-20 13:29:35 +0100

    add utf-8 support in response

--- pyris/api/run.py
+++ pyris/api/run.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import sys
+import settings
 from pyris.api import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
     app.run(port=5555, debug=True)
